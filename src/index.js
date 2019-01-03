cc5a3a820a82a60440a1dd059b1ef06b458a6343
Aris Goudouras <aris.gk@gmail.com>
2019-01-03 15:20:37 +0200

    Include interim results

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const request = {
     sampleRateHertz: sampleRateHertz,
     languageCode: languageCode,
   },
-  interimResults: false, // If you want interim results, set this to true
+  interimResults: true, // If you want interim results, set this to true
 };
 
 // Create a recognize stream
